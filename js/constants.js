902db0aab2c6812c39559534ab526371235e2f07
Robert Benjamin Osborne <Robert@Roberts-MacBook-Air-2.local>
2018-08-18 21:46:10 -0400

    Modified client commmunication'

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+export const COMMAND = 'Command';
+export const REGISTER_DEVICE = 'Register Device';
+export const DEVICE = {
+    DEVICE: 'Device',
+    UNIQUE_IDENTIFIER: 'Unique Identifier',
+    CODE: 'Code',
+    DESCRIPTION: 'Description'
+}
--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-// https://pastebin.com/twP1Ksv4
+import {COMMAND, REGISTER_DEVICE, DEVICE} from 'constants';
 
-var mySocket = new WebSocket("ws://localhost:9000/ws");
+let server_address = window.location.hostname;
+let mySocket = new WebSocket("ws://" + server_address + ":9000/ws");
 
 window.addEventListener("load", function () {
     // create websocket instance
 
     // add event listener reacting when message is received
     mySocket.onmessage = function (event) {
-        var output = document.getElementById("output");
-        // put text into our output div
-        //output.textContent = event.data;
-
         msg = JSON.parse(event.data);
-        //var output = document.getElementById("text_output");
+        console.log(msg)
 
         if (msg.cmd === 'AUTHENTICATE') {
             text_login_status.textContent = msg.status;
@@ -24,28 +21,43 @@ window.addEventListener("load", function () {
         }
 
     };
-    //var form = document.getElementsByClassName("foo");
+    //let form = document.getElementsByClassName("foo");
 });
+
+document.getElementById("button_register").addEventListener("click", function () {
+    let client_msg = {
+        COMMAND: REGISTER_DEVICE, 
+        [DEVICE.DEVICE]: {
+            [DEVICE.NAME]: 'Website',
+            [DEVICE.CODE]: 'AD',
+            [DEVICE.DESCRIPTION]: 'Admin accessing PixelPi from website'
+        },
+        
+    }
+
+    mySocket.send(JSON.stringify(client_msg))
+    //e.preventDefault()
+})
 
 
 document.getElementById("button_create_member").addEventListener("click", function () {
-    var text_new_username_input = document.getElementById("text_new_username_input");
-    var text_new_password_input = document.getElementById("text_new_password_input");
+    let text_new_username_input = document.getElementById("text_new_username_input");
+    let text_new_password_input = document.getElementById("text_new_password_input");
 
     mySocket.send(JSON.stringify({cmd: 'CREATE_USER', username: text_new_username_input.value, password: text_new_password_input.value}))
     //e.preventDefault()
 })
 
 document.getElementById("button_sign_in").addEventListener("click", function () {
-    var text_existing_username_input = document.getElementById("text_existing_username_input");
-    var text_existing_password_input = document.getElementById("text_existing_password_input");
+    let text_existing_username_input = document.getElementById("text_existing_username_input");
+    let text_existing_password_input = document.getElementById("text_existing_password_input");
 
     mySocket.send(JSON.stringify({cmd: 'AUTHENTICATE', username: text_existing_username_input.value, password: text_existing_password_input.value}))
     //e.preventDefault()
 })
 
 document.getElementById("button_send_chat").addEventListener("click", function () {
-    var text_chat_input = document.getElementById("text_chat_input");
+    let text_chat_input = document.getElementById("text_chat_input");
     
     input_text = text_chat_input.value;
     mySocket.send(JSON.stringify({cmd: 'MESSAGE', text: input_text}))
